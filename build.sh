eb2e93a53fc5737358aa194929a0d2f016cd50db
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-07-14 20:30:25 +0200

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #! /bin/sh
-docker build --no-cache --tag ghcr.io/priestofadanos/lc_arm64:latest --file  docker/arm.Dockerfile --platform  linux/arm64/v8 docker/
-docker build --no-cache --tag ghcr.io/priestofadanos/lc_amd:latest --file  docker/amd.Dockerfile --platform linux/amd64 docker/
+docker build --no-cache --tag ghcr.io/priestofadanos/lc_amd:latest --file  docker/Dockerfile --platform linux/amd64 docker/
