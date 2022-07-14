e2e9749c8eb4f2903991638725a6fc222471d5cb
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-07-14 11:54:49 +0200

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-docker build --no-cache --platform linux/arm64/v8 --tag push ghcr.io/PriestOfAdanos/lc_arm64:latest docker/
-docker build --no-cache --platform linux/amd64 --tag push ghcr.io/PriestOfAdanos/lc_amd:latest docker/
+docker build --no-cache --platform linux/arm64/v8 --tag push ghcr.io/PriestOfAdanos/lc_arm64:latest
+docker build --no-cache --platform linux/amd64 --tag push ghcr.io/PriestOfAdanos/lc_amd:latest
