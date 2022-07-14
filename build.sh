0119fcc803736daf9987be7a15a9886297fb73fd
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-07-14 11:53:56 +0200

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-docker build --no-cache --platform linux/arm64/v8 --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)  --tag push ghcr.io/PriestOfAdanos/lc_arm64:latest docker/
-docker build --no-cache --platform linux/amd64 --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)  --tag push ghcr.io/PriestOfAdanos/lc_amd:latest docker/
+docker build --no-cache --platform linux/arm64/v8 --tag push ghcr.io/PriestOfAdanos/lc_arm64:latest docker/
+docker build --no-cache --platform linux/amd64 --tag push ghcr.io/PriestOfAdanos/lc_amd:latest docker/
