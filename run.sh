4ed04911d3b59a1733da70b3efbe770e39fb6cf7
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-07-14 19:46:09 +0200

    change image tag

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-docker run -it  -v /home/phabrzyk/lc/ws:/home/lc/ws -v /home/phabrzyk/.ssh:/home/lc/.ssh lc:latest bash 
+docker run -it  -v /home/phabrzyk/lc/ws:/home/lc/ws -v /home/phabrzyk/.ssh:/home/lc/.ssh ghcr.io/lc/lc_amd:latest bash 
 # --device=/dev/ttyUSB0
