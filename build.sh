5ac19e4e4c7d4f539c7c67922a1060291f34d7c8
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-07-20 17:07:55 +0200

    some changes

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker build --no-cache --tag ghcr.io/priestofadanos/lc_amd:latest --file  docker/Dockerfile --platform linux/amd64 docker/
+docker build --no-cache --tag ghcr.io/priestofadanos/lc_amd:latest --file  docker/Dockerfile docker/
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-docker run -it  --privileged --user=root --mount type=bind,source="$(pwd)"/bags,target=/bags  ghcr.io/priestofadanos/lc:image-push
+docker run -it --net=host --privileged --user=root --mount type=bind,source="$(pwd)"/ws,target=/home/lc/ws --mount type=bind,source="$(pwd)"/bags,target=/bags  ghcr.io/priestofadanos/lc:image-push
 # --device=/dev/ttyUSB0
