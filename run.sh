6c5d7330bdbd5de1127f3ff4f5f486fa8b1d1b8f
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-08-27 09:38:58 +0200

    add a production flag

--- run.sh
+++ run.sh
@@ -1,7 +1,35 @@
 #! /bin/sh
-docker run -it --net=host --privileged --user=root --mount type=bind,source="$(pwd)"/ws,target=/home/lc/ws \
---mount type=bind,source="$(pwd)"/bags,target=/bags  \
---mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
---device=/dev/ttyUSB0 \
-ghcr.io/priestofadanos/lc:image-push ./run_server.sh
-# --device=/dev/ttyUSB0
+
+forward_parameters="--mount type=bind,source="$(pwd)"/ws,target=/home/lc/ws"
+trailing_parameters=''
+
+print_usage() {
+  printf "Usage: 
+  production_mode: ./run.sh -p
+  development_mode: ./run.sh -d
+  "
+}
+run_docker() {
+  printf "running docker ..."
+  docker run -it --net=host --privileged --user=root  $forward_parameters \
+    --mount type=bind,source="$(pwd)"/bags,target=/bags  \
+    --mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
+    --device=/dev/ttyUSB0 \
+    ghcr.io/priestofadanos/lc:image-push ./run_server.sh \
+    $trailing_parameters
+}
+
+while getopts 'pd' flag; do
+  case "${flag}" in
+    p) trailing_parameters='--device=/dev/ttyUSB0 ' 
+       forward_parameters=''
+       run_docker
+    ;;
+    d) run_docker
+    ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+
