f8aec2b46e3db26d1920c8e9764d6eef8ee9dc97
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2022-08-28 10:38:35 +0200

    move scripts to docker folder

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ run_docker() {
     --mount type=bind,source="$(pwd)"/bags,target=/bags  \
     --mount type=bind,source=/var/run/dbus/system_bus_socket,target=/var/run/dbus/system_bus_socket \
     --device=/dev/ttyUSB0 \
-    ghcr.io/priestofadanos/lc:image-push ./run_server.sh \
+    ghcr.io/priestofadanos/lc:image-push bash \
     $trailing_parameters
 }
 
